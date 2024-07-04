6235a4cae2b521be75b2402957e67c9463bed4ef
Aleksei Bychkov <nwsm@bk.ru>
2024-07-05 02:21:52 +0300

    Добавляет правки модального окна

--- scripts/modal.js
+++ scripts/modal.js
@@ -5,17 +5,17 @@
     modalWindow = document.querySelector('.modal-container');
 
     // Открываем модальное окно
-    modalTrigger.addEventListener('click', () => {
+    modalTrigger.onclick = function() {
       modalWindow.style.display = 'flex';
-    });
+    };
 
     // Закрываем модальное окно
-    modalCloseBtn.addEventListener('click', () => {
+    modalCloseBtn.onclick = function() {
       modalWindow.style.display = 'none';
-    });
+    };
 
     // Закрываем по кнопке ESC
-    document.addEventListener('keydown', function(event) {
+    window.addEventListener('keydown', function(event) {
       const key = event.key;
       if (key === "Escape") {
         modalWindow.style.display = 'none';
